d7a57e1bb44aa200078ce8425dcc613de8b926d9
Craftinators <craftinatorssx@gmail.com>
2024-01-13 06:21:23 -0500

    refactor(Base64Texture): switch to utility class

--- src/main/java/me/craftinators/harbor/utilities/Base64Texture.java
+++ src/main/java/me/craftinators/harbor/utilities/Base64Texture.java
@@ -8,20 +8,12 @@ import org.bukkit.inventory.meta.SkullMeta;
 import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.UUID;
 
-public enum Base64Texture {
-    ;
+public final class Base64Texture {
+    private Base64Texture() {}
 
-    private final String base64;
-
-    Base64Texture(String base64) {
-        this.base64 = base64;
-    }
-
-    public @NotNull ItemStack getTexturedHead() {
+    public static @NotNull ItemStack getTexturedHeadFromBase64(final @NotNull String base64) {
         ItemStack playerHead = new ItemStack(Material.PLAYER_HEAD);
         SkullMeta playerHeadMeta = (SkullMeta) playerHead.getItemMeta();
 
@@ -29,17 +21,11 @@ public enum Base64Texture {
         profile.getProperties().put("textures", new Property("textures", base64));
 
         try {
-            Method methodSetProfileMethod = playerHeadMeta.getClass().getDeclaredMethod("setProfile", GameProfile.class);
-            methodSetProfileMethod.setAccessible(true);
-            methodSetProfileMethod.invoke(playerHeadMeta, profile);
-        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException methodExceptions) {
-            try {
-                Field metaProfileField = playerHeadMeta.getClass().getDeclaredField("profile");
-                metaProfileField.setAccessible(true);
-                metaProfileField.set(playerHeadMeta, profile);
-            } catch (NoSuchFieldException | IllegalAccessException fieldExceptions) {
-                fieldExceptions.printStackTrace();
-            }
+            Field metaProfileField = playerHeadMeta.getClass().getDeclaredField("profile");
+            metaProfileField.setAccessible(true);
+            metaProfileField.set(playerHeadMeta, profile);
+        } catch (NoSuchFieldException | IllegalAccessException fieldExceptions) {
+            fieldExceptions.printStackTrace();
         }
 
         playerHead.setItemMeta(playerHeadMeta);
